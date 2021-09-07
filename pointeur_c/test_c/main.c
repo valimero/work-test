c1fe8868e22e2c79eb192041580375905a9a570e
Valentin COPPIN <valentin.coppin@ecole2600.com>
2021-09-07 04:57:55 -0400

    add comment

--- pointeur_c/test_c/main.c
+++ pointeur_c/test_c/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
-void ft_rec(char str[], int *error)
+// (int *) --> Indique que "error" est l'adresse d'un int 
+void ft_rec(char str[], int *pt_error)
 {
 	if(str[0] == '\0') return;
-	(*error)++;
-	ft_rec(str + 1, error);
+	
+	// pt_error 		--> Adresse du int
+	// (*pt_error) 		--> permet d'accéder à la valeur pointée par "error"
+	// (*pt_error)++ 	--> permet d'incrémenter la valeur pointé par "pt_error" 
+	(*pt_error)++;
+	ft_rec(str + 1, pt_error);
 }
 
 
@@ -16,10 +21,9 @@ int main()
 	int error;
 	error = 0;
 
+	// &error 		--> Représente l'adresse de "error" (int) 
 	ft_rec(str, &error);
 
 	printf("%d\n", error);
-	
-	//ft_rec(* (str + 1), &error);
 }
 
